b7d4636737c0f633283d2b6e054527a730203dda
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-10 00:51:26 +0800

    Polish mobile player movement and add forgiving jump timing

--- lowpoly-android/scripts/player.gd
+++ lowpoly-android/scripts/player.gd
@@ -1,14 +1,19 @@
 extends CharacterBody3D
 
-@export var speed: float = 5.0
-@export var acceleration: float = 12.0
-@export var jump_velocity: float = 4.5
+@export var speed: float = 5.6
+@export var acceleration: float = 18.0
+@export var air_acceleration: float = 8.0
+@export var jump_velocity: float = 4.9
 @export var look_sensitivity: float = 0.003
+@export var coyote_time: float = 0.12
+@export var jump_buffer_time: float = 0.12
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var pitch: float = 0.0
 var mobile_move_vector: Vector2 = Vector2.ZERO
 var mobile_jump_requested: bool = false
+var coyote_timer: float = 0.0
+var jump_buffer_timer: float = 0.0
 
 @onready var head: Node3D = $Head
 
@@ -28,24 +33,33 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
+	if is_on_floor():
+		coyote_timer = coyote_time
+	else:
+		coyote_timer = max(coyote_timer - delta, 0.0)
 		velocity.y -= gravity * delta
 
-	if (Input.is_action_just_pressed("jump") or mobile_jump_requested) and is_on_floor():
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("jump") or mobile_jump_requested:
+		jump_buffer_timer = jump_buffer_time
 	mobile_jump_requested = false
+	jump_buffer_timer = max(jump_buffer_timer - delta, 0.0)
+
+	if jump_buffer_timer > 0.0 and coyote_timer > 0.0:
+		velocity.y = jump_velocity
+		jump_buffer_timer = 0.0
+		coyote_timer = 0.0
 
 	var keyboard_input := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var input_dir := keyboard_input
 	if mobile_move_vector.length() > 0.05:
-		input_dir = Vector2(mobile_move_vector.x, mobile_move_vector.y)
+		input_dir = mobile_move_vector.limit_length(1.0)
 
 	var direction := (global_transform.basis * Vector3(input_dir.x, 0.0, input_dir.y)).normalized()
-
+	var accel := acceleration if is_on_floor() else air_acceleration
 	var target_x := direction.x * speed
 	var target_z := direction.z * speed
-	velocity.x = move_toward(velocity.x, target_x, acceleration * delta)
-	velocity.z = move_toward(velocity.z, target_z, acceleration * delta)
+	velocity.x = move_toward(velocity.x, target_x, accel * delta)
+	velocity.z = move_toward(velocity.z, target_z, accel * delta)
 
 	move_and_slide()
 
@@ -53,6 +67,10 @@ func _apply_look(delta: Vector2) -> void:
 	rotate_y(-delta.x)
 	pitch = clamp(pitch - delta.y, deg_to_rad(-80), deg_to_rad(80))
 	head.rotation.x = pitch
+
+func teleport_to(pos: Vector3) -> void:
+	global_position = pos
+	velocity = Vector3.ZERO
 
 func get_echo_rotation() -> Vector3:
 	return Vector3(0.0, global_rotation.y, 0.0)
